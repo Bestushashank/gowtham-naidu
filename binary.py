889921b75d4aba5f392d0336c1e63b18b2b32844
Bestushashank <109731653+Bestushashank@users.noreply.github.com>
2022-08-27 07:13:47 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,25 @@
+
+from timeit import default_timer
+pos=-1
+def search(list,n):
+  l=0
+  u=len(list)
+  while l<=u:
+    mid=(l+u)//2
+    if list[mid]==n:
+       globals()["pos"]=mid
+       return True
+    else:
+        if list[mid]<n:
+            l=mid+1;
+        else:
+            u=mid-1;
+start = default_timer()
+list=[1,2,3,4,5,6]
+n=5
+if search(list,n):
+ print("found at",pos)
+ duration = default_timer() - start
+ print("found in", duration, "seconds")
+else:
+ print("not found")
